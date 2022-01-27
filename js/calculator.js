0484d125dcde01e96a0e70c3353c71ffc98389f3
DGAB <diegoaybar2021@gmail.com>
2022-01-27 16:39:01 -0500

    Icon and Format
    Descripction Empty

--- js/calculator.js
+++ js/calculator.js
@@ -5,6 +5,7 @@ resultado.innerHTML = 0;
 function give() {
   resultado.innerHTML = Math.round((n1.value * 7) / 10);
 }
+
 function recive() {
   resultado.innerHTML = Math.round((n1.value * 10) / 7);
 }
